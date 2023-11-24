80e1ddf5f6a6388e862bff7b5a138854794d2e49
Kenyi Asencios Matias <asenciosm.k@gmail.com>
2023-11-24 10:51:14 -0500

    configurnado variables de entorno

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,25 @@
-const { config } = require('dotenv')
-config()
+require('dotenv').config()
 
+let db = {}
 
-module.exports = {
-    db: {
+if (process.env.NODE_ENV === 'production') {
+    db = {
+        user: process.env.DB_PROD_USER,
+        password: process.env.DB_PROD_PASSWORD,
+        host: process.env.DB_PROD_HOST,
+        port: process.env.DB_PROD_PORT,
+        database: process.env.DB_PROD_NAME,
+    }
+} else {
+    db = {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         database: process.env.DB_NAME
     }
+
 }
+
+
+module.exports = { db }
