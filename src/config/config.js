9e77ba80aea400bb6669b31c3d0a78e46566ef1a
Kenyi Asencios Matias <asenciosm.k@gmail.com>
2023-11-25 17:30:43 -0500

    corrigiendo la configuracion de la base de datos

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,12 @@
-require('dotenv').config()
+const { config } = require('dotenv')
+config()
 
-let db = {}
-
-if (process.env.NODE_ENV === 'production') {
-    db = {
-        user: process.env.DB_PROD_USER,
-        password: process.env.DB_PROD_PASSWORD,
-        host: process.env.DB_PROD_HOST,
-        port: process.env.DB_PROD_PORT,
-        database: process.env.DB_PROD_NAME,
-    }
-} else {
-    db = {
+module.exports = {
+    db: {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         database: process.env.DB_NAME
     }
-
 }
-
-
-module.exports = { db }
